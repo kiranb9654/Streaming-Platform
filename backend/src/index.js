ea97ec0ce92063f1d85d73e4379b7dbc8b871d0e
kiran <kiranbagri203@gmail.com>
2025-10-24 12:50:08 +0530

    add user and video models

---
+++ backend/src/index.js
@@ -0,0 +1,52 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import {app} from './app.js'
+
+
+
+dotenv.config({
+    path: './.env'
+})
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express()
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.DB_URL}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("App not able to connect with database", error);
+            throw error 
+        })
+        app.listen(process.env.PORT, ()=> {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error(" DB ERROR: ", error)
+        throw err
+    }
+})()
+*/
+
